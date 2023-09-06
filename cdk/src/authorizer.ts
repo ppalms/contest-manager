6b994558b371b553b1bb9552d4b929fe33fb09d4
Patrick Palmer <ppalmer@pm.me>
2023-09-05 23:42:17 -0500

    Clean up authorizer

--- cdk/src/authorizer.ts
+++ cdk/src/authorizer.ts
@@ -1,12 +1,18 @@
 import * as jwt from 'jsonwebtoken';
 
-export async function handler(event: any, _context: any): Promise<any> {
+export async function handler(
+  event: any,
+  _context: any
+): Promise<{ isAuthorized: boolean; resolverContext?: any }> {
   try {
     const tenantId = getTenantId(event.authorizationToken);
-    if (!tenantId)
+    if (!tenantId) {
       return {
         isAuthorized: false,
       };
+    }
+
+    // TODO get tenant-scoped STS token
 
     return {
       isAuthorized: true,
@@ -16,32 +22,29 @@ export async function handler(event: any, _context: any): Promise<any> {
     };
   } catch (error) {
     console.error('Authorization failed:', error);
-    throw new Error('Unauthorized');
+    return {
+      isAuthorized: false,
+    };
   }
 }
 
-function getTenantId(token: string): string | null {
-  try {
-    const decoded = jwt.decode(token, { complete: true });
+function getTenantId(token: string): string {
+  const decoded = jwt.decode(token, { complete: true });
 
-    if (!decoded || typeof decoded !== 'object' || !decoded.payload) {
-      throw new Error('Invalid token');
-    }
+  if (!decoded || typeof decoded !== 'object' || !decoded.payload) {
+    throw new Error('Invalid token');
+  }
 
-    // TODO verify JWT with Cognito
+  // TODO verify JWT with Cognito
 
-    const payload = decoded.payload as jwt.JwtPayload;
-    if (!payload) {
-      throw new Error('Invalid token');
-    }
+  const payload = decoded.payload as jwt.JwtPayload;
+  if (!payload) {
+    throw new Error('Invalid token');
+  }
 
-    if (payload['custom:tenantId']) {
-      return payload['custom:tenantId'];
-    } else {
-      throw new Error('tenantId not found in token');
-    }
-  } catch (error) {
-    console.error('Failed to validate and extract tenantId', error);
-    return null;
+  if (payload['custom:tenantId']) {
+    return payload['custom:tenantId'];
+  } else {
+    throw new Error('tenantId not found in token');
   }
 }
