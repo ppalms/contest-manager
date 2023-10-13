bc2efa6cfa51c60ee28552b54006feaaa52cb57c
Patrick Palmer <ppalmer@pm.me>
2023-10-12 23:36:27 -0500

    Adjust organization loading UX

--- src/app/(admin)/organizations/[id]/page.tsx
+++ src/app/(admin)/organizations/[id]/page.tsx
@@ -141,7 +141,7 @@ export default function OrganizationDetail({ params }: any) {
 
   return (
     <>
-      <div className="px-4 sm:px-6 lg:px-8 divide-y">
+      <div className="px-4 sm:px-6 lg:px-8 divide-y flex flex-col flex-grow">
         <div className="pb-10">
           <form onSubmit={(e) => handleSaveOrg(e)}>
             <div className="px-4 sm:px-0 flex items-center justify-between">
@@ -209,13 +209,16 @@ export default function OrganizationDetail({ params }: any) {
                   <select
                     id="type"
                     name="type"
-                    className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    value={organization?.type || OrganizationType.School}
+                    className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:ring-0 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-300"
+                    value={organization?.type || OrganizationType.Unknown}
                     onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
                       const type = e.target.value as OrganizationType;
                       setOrganization({ ...organization!, type });
                       setIsValid(true);
                     }}>
+                    <option value={OrganizationType.Unknown}>
+                      {loading ? '' : 'Select a type'}
+                    </option>
                     <option value={OrganizationType.State}>
                       {orgTypeMap[OrganizationType.State]}
                     </option>
@@ -232,31 +235,37 @@ export default function OrganizationDetail({ params }: any) {
           </form>
         </div>
 
-        {organization?.id && (
-          <div>
-            {/* TODO move into user list component */}
-            <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="sm:col-span-6">
-                <h3 className="text-base font-semibold leading-7 text-gray-900">
-                  Users
-                </h3>
+        <div className="flex-grow">
+          {!loading && organization?.id ? (
+            <>
+              {/* TODO move into user list component */}
+              <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-6">
+                  <h3 className="text-base font-semibold leading-7 text-gray-900">
+                    Users
+                  </h3>
+                </div>
               </div>
-            </div>
 
-            <UserList
-              users={users}
-              organizationId={organization.id}
-              onUserSaved={(user) => {
-                setNotificationTitle('Successfully saved!');
-                setNotificationMessage(
-                  `${user.firstName} ${user.lastName} saved`
-                );
-                setNotificationType('success');
-                setShowNotification(true);
-              }}
-            />
-          </div>
-        )}
+              <UserList
+                users={users}
+                organizationId={organization.id}
+                onUserSaved={(user) => {
+                  setNotificationTitle('Successfully saved!');
+                  setNotificationMessage(
+                    `${user.firstName} ${user.lastName} saved`
+                  );
+                  setNotificationType('success');
+                  setShowNotification(true);
+                }}
+              />
+            </>
+          ) : (
+            <div className="flex justify-center h-full pt-32">
+              <div className="animate-spin rounded-full h-24 w-24 border-b-2 border-indigo-700" />
+            </div>
+          )}
+        </div>
       </div>
 
       {showNotification && (
