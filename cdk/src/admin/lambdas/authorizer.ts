d61bd6b474b51a0cddb006f84e86b908d08c1ab4
Patrick Palmer <ppalmer@pm.me>
2023-11-02 00:45:46 -0500

    Fix build error

--- cdk/src/admin/lambdas/authorizer.ts
+++ cdk/src/admin/lambdas/authorizer.ts
@@ -11,29 +11,28 @@ export async function handler(
   request: Request,
   _: any
 ): Promise<{ isAuthorized: boolean; resolverContext?: any }> {
+  const decoded = jwt.decode(request.authorizationToken, { complete: true });
+  const payload = decoded?.payload as jwt.JwtPayload;
+  if (!payload) {
+    throw new Error('Invalid JWT token');
+  }
+
+  const tenantId = payload['custom:tenantId'];
+  if (!tenantId || tenantId.length === 0) {
+    return {
+      isAuthorized: false,
+    };
+  }
+
+  // TODO store tenant context in S3/cloudfront and get rid of SSM params
+  const getUserPoolIdCommand = new GetParametersCommand({
+    Names: ['/shared/user-pool-id'],
+  });
+
+  const ssmClient = new SSMClient({ region: process.env.AWS_REGION });
+  const ssmResult = await ssmClient.send(getUserPoolIdCommand);
+  const userPoolId = ssmResult.Parameters![0].Value!;
   try {
-    const decoded = jwt.decode(request.authorizationToken, { complete: true });
-    const payload = decoded?.payload as jwt.JwtPayload;
-    if (!payload) {
-      throw new Error('Invalid JWT token');
-    }
-
-    const tenantId = payload['custom:tenantId'];
-    if (!tenantId || tenantId.length === 0) {
-      return {
-        isAuthorized: false,
-      };
-    }
-
-    // TODO store tenant context in S3/cloudfront and get rid of SSM params
-    const getUserPoolIdCommand = new GetParametersCommand({
-      Names: ['/shared/user-pool-id'],
-    });
-
-    const ssmClient = new SSMClient({ region: process.env.AWS_REGION });
-    const ssmResult = await ssmClient.send(getUserPoolIdCommand);
-    const userPoolId = ssmResult.Parameters![0].Value!;
-
     await verifyJwt();
 
     // TODO get tenant-scoped STS token
@@ -75,47 +74,47 @@ export async function handler(
         userPoolId: userPoolId,
       },
     };
-
-    // Local functions
-    type JWK = {
-      alg: string;
-      e: string;
-      kid: string;
-      kty: 'RSA';
-      n: string;
-      use: string;
-    };
-
-    type JWKS = {
-      keys: JWK[];
-    };
-
-    async function getJwks(): Promise<JWKS> {
-      const jwksUrl = `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
-
-      // TODO gimme da caaccchechehchecchhe
-      return new Promise((resolve, reject) => {
-        https.get(jwksUrl, (response) => {
-          let data = '';
-          response.on('data', (chunk) => (data += chunk));
-          response.on('end', () => resolve(JSON.parse(data)));
-          response.on('error', reject);
-        });
-      });
-    }
-
-    async function verifyJwt() {
-      const jwks = await getJwks();
-      const kid = decoded!.header.kid;
-      const jwk = jwks.keys.find((key) => key.kid === kid);
-      const pem = jwkToPem(jwk!);
-
-      jwt.verify(request.authorizationToken, pem, { algorithms: ['RS256'] });
-    }
   } catch (error) {
     console.error('Authorization failed', error);
     return {
       isAuthorized: false,
     };
+  }
+
+  // Local functions
+  type JWK = {
+    alg: string;
+    e: string;
+    kid: string;
+    kty: 'RSA';
+    n: string;
+    use: string;
+  };
+
+  type JWKS = {
+    keys: JWK[];
+  };
+
+  async function getJwks(): Promise<JWKS> {
+    const jwksUrl = `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
+
+    // TODO gimme da caaccchechehchecchhe
+    return new Promise((resolve, reject) => {
+      https.get(jwksUrl, (response) => {
+        let data = '';
+        response.on('data', (chunk) => (data += chunk));
+        response.on('end', () => resolve(JSON.parse(data)));
+        response.on('error', reject);
+      });
+    });
+  }
+
+  async function verifyJwt() {
+    const jwks = await getJwks();
+    const kid = decoded!.header.kid;
+    const jwk = jwks.keys.find((key) => key.kid === kid);
+    const pem = jwkToPem(jwk!);
+
+    jwt.verify(request.authorizationToken, pem, { algorithms: ['RS256'] });
   }
 }
