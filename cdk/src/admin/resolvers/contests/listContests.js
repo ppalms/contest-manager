f45b6db862bc8a3750ceb7229f4bbfa3846a229d
Patrick Palmer <ppalmer@pm.me>
2023-11-07 23:31:15 -0600

    Add type and class filters to listContests

--- cdk/src/admin/resolvers/contests/listContests.js
+++ cdk/src/admin/resolvers/contests/listContests.js
@@ -1,20 +1,42 @@
+import { util } from '@aws-appsync/utils';
+
 export function request(ctx) {
   const tenantId = ctx.identity?.resolverContext.tenantId ?? '001';
+
+  const query = {
+    expression: 'GSI1PK = :pk',
+    expressionValues: util.dynamodb.toMapValues({
+      ':pk': `TENANT#${tenantId}#CONTESTS`,
+    }),
+  };
+
+  if (ctx.arguments.type) {
+    query.expression += ' and GSI1SK = :sk';
+    query.expressionValues[':sk'] = util.dynamodb.toString(ctx.arguments.type);
+  }
 
   return {
     operation: 'Query',
     index: 'GSI1',
-    query: {
-      expression: 'GSI1PK = :pk',
-      expressionValues: util.dynamodb.toMapValues({
-        ':pk': `TENANT#${tenantId}#CONTESTS`,
-      }),
-    },
+    query: query,
   };
 }
 
 export function response(ctx) {
-  const contests = ctx.result.items.map((entity) => {
+  let contestItems = ctx.result.items;
+
+  if (
+    Array.isArray(ctx.arguments.classes) &&
+    ctx.arguments.classes.length > 0
+  ) {
+    contestItems = contestItems.filter((contest) => {
+      return ctx.arguments.classes.some((contestClassFilter) =>
+        contest.eligibleClasses.includes(contestClassFilter)
+      );
+    });
+  }
+
+  const contests = contestItems.map((entity) => {
     return {
       ...entity,
       id: entity.PK.split('#')[3],
